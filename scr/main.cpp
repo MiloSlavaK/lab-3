54eb2ff613dcffb7f249a8d2c5a20447f9fda25b
Miloslava Kislitsyna <90026628+MiloSlavaK@users.noreply.github.com>
2025-04-04 11:29:59 +0300

    Update main.cpp

--- scr/main.cpp
+++ scr/main.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
-
-int main() {}
+class Cat{
+public:
+  Cat() {};
+  ~Cat() {};
+};
+class Dog{
+public:
+  Dog() {};
+  ~Dog() {};
+};
+class Animals {
+public:
+    Animals() {
+        a = new Cat();
+        b = new Dog();
+    }
+    
+    ~Animals() {}
+    
+private:
+    Cat* a;
+    Dog* b;
+};
+int main() {
+Animals vet;
+return 0;
+}
